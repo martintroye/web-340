86c4b5232b3dc6fa7e98b1dd39981147b4f57eab
Troy Martin <troyemartin@gmail.com>
2019-05-10 15:58:32 -0500

    Exercise 4.2

---
+++ week-4/json-api/martin-exercise-4_2.js
@@ -0,0 +1,45 @@
+/*
+============================================
+; Title: Assignment 4.2
+; Author: Troy Martin
+; Date: 05/10/2019
+; Modified By: Troy Martin
+; Description: JSON Apis
+;===========================================
+*/
+// start program
+// Require statement that imports the header.js file from my root directory.
+const header = require("../../header.js");
+// Call the console.log() function and output a well-formatted header with a line feed
+console.log(header.display("Troy", "Martin", "Assignment 4.2"));
+console.log("");
+
+// variable declaration and assignment
+let tractor = {
+  id: 1,
+  type: "Tractor",
+  make: "John Deere"
+};
+
+// Declare the express variable and import the express module
+var express = require("express");
+// Declare the http variable and import the http module
+var http = require("http");
+
+// Declare the app variable and call the express function to start an Express application instance
+var app = express();
+
+// Call the get function to set the route handler for the home page
+app.get("/vehicles/:id", function(request, response){
+  var vehicleId = parseInt(request.params.id, 10);
+  tractor.id = vehicleId;
+  response.json(tractor);
+});
+
+// Call the createServer method passing the application and listen for a request on port 8080 with a listening function
+http.createServer(app).listen(8080, function(){
+  // Call the console log function to output a message
+  console.log("Application started on port 8080.")
+});
+
+// end program
